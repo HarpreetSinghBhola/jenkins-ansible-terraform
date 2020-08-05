58b812ed485195b83dda82945d2a90bffbf63799
HarpreetSinghBhola <harpreetbho@basware.com>
2020-08-06 00:06:39 +0530

    Update backend.tf
    test

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     profile = "default"
     region  = "us-east-1"
-    bucket          = "tfstate-bucket-har"
+    bucket          = "tfstate-bucket-harry"
     key     = "terraform.tfstate"
   }
 }
